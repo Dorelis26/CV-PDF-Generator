cd4a949e2179aa8423fad7201183f4a1138e66f4
Nicolas Dorelis <115403319+Dorelis26@users.noreply.github.com>
2023-03-20 09:07:59 -0300

    Add files via upload

---
+++ mysite/pdf/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from .models import Profile
+import pdfkit
+from django.http import HttpResponse
+from django.template import loader
+import io 
+# Create your views here.
+
+def accept(request):
+    if request.method == "POST":
+        name = request.POST.get("name","")
+        email= request.POST.get("email","")
+        phone = request.POST.get("phone","")
+        summary = request.POST.get("summary","")
+        degree = request.POST.get("degree","")
+        education = request.POST.get("education","")
+        university= request.POST.get("university","")
+        previous_experience = request.POST.get("previous_experience","")
+        skills = request.POST.get("skills","")
+        profile = Profile(name=name,email=email,phone=phone,summary=summary,degree=degree,education=education,university=university,previous_experience=previous_experience,skills=skills)
+        profile.save()
+    return render(request,'pdf/accept.html')
+
+def cv(request,id):
+    user_profile = Profile.objects.get(pk=id)
+    template = loader.get_template('pdf/resume.html')
+    html = template.render({'user_profile':user_profile})
+    options ={
+        'page-size':'Letter',
+        'encoding':"UTF-8",
+    }
+    #SET TO UR PATH
+    config = pdfkit.configuration(wkhtmltopdf=r'C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe')
+ 
+    #Must be 'option=options' and add 'configuration=config'
+    pdf = pdfkit.from_string(html,False,options=options,configuration=config)
+    response = HttpResponse(pdf,content_type='application/pdf')
+ 
+    #filename must be included in contentdisposition
+    response['Content-Disposition'] ='attachment;filename=cv.pdf'
+ 
+    return response
+
+def list(request):
+    profile = Profile.objects.all()
+    return render(request,'pdf/list.html',{'profile':profile})
